fc2df2918a116b9046d3eb3f2cf6d0e59c3c64f4
Victor Portella <visportella@gmail.com>
2015-05-26 23:55:18 +1000

    Corrections and new features added to the world model

--- src/MapModel.java
+++ src/MapModel.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+
 
 public class MapModel {
-
-	
    final static int EAST   = 0;
    final static int NORTH  = 1;
    final static int WEST   = 2;
    final static int SOUTH  = 3;	
    
+   final static char FLAT = ' ';
+   final static char WATER = '~';
+   final static char WALL = '*';
+   final static char TREE = 'T';
+   final static char AXE = 'a';
+   final static char TNT = 'd';
+   final static char BOAT = 'B';
+   final static char GOLD = 'g';
+   
    private char[][] map;
    private int lin,col, dir;
    private int minLin, minCol, maxLin, maxCol;
+   private boolean inBoat;
+   private boolean hasAxe;
+   private boolean hasGold;
+   private int numberTNT;
+   private ArrayList<Point> listTNTs;
+   private ArrayList<Point> listBoats;
+   private ArrayList<Point> listAxes;
+   Point goldPos;
+   
+   
+/* ********* CONSTRUCTOR ************* */
    
    public MapModel()
    {
@@ -21,15 +41,69 @@ public class MapModel {
 	
 	   //Our agent will start at the center of the map
 	   lin = col = 79;
-	   map[lin][col] = ' ';
+	   map[lin][col] = FLAT;
 	   
 	   // Variable for print control
 	   minLin = minCol = maxLin = maxCol = 79;
+	   
+	   //It starts outside the boat
+	   inBoat = false;
+	   
+	   //Starts with no tools
+	   numberTNT = 0; hasAxe = false;
+	   
+	   //Initialize the lists of tools with empty lists
+	   listTNTs = new ArrayList<Point>();
+	   listBoats = new ArrayList<Point>();
+	   listAxes = new ArrayList<Point>();
+	   goldPos = null;
 	   
 	   
 	   // We adopt the north as the start position direction
 	   dir = NORTH;
    }
+   
+   /* ********** PUBLIC SETTERS AND GETTERS ************ */
+   
+   public char map(int l, int c)
+   {
+	   return map[l][c];
+   }
+   
+   public boolean hasAxe()
+   {
+	   return hasAxe;
+   }
+   
+   public int numberTNT()
+   {
+	   return numberTNT;
+   }
+   
+   public int minLinVisible()
+   {
+	   return minLin;
+   }
+   
+   public int minColVisible()
+   {
+	   return minCol;
+   }
+   
+   public int maxLinVisible()
+   {
+	   return maxLin;
+   }
+   
+   public int maxColVisible()
+   {
+	   return maxCol;
+   }
+   
+   
+   /*****************************************************/
+   
+   
    
    private void setPos(int lin, int col, char ch)
    {
@@ -40,6 +114,29 @@ public class MapModel {
 	   if(col < minCol) minCol = col;
 	   if(lin > maxLin) maxLin = lin;
 	   if(col > maxCol) maxCol = col;
+	   
+	   //If this position was unknown, we may have
+	   //found something interesting (A tool or gold)
+	   
+	   if(map[lin][col] == '?')
+	   {
+		   switch(ch)
+		   {
+		   	case AXE:
+		   		listAxes.add(new Point(lin,col));
+		   		break;
+		   	case TNT:
+		   		listTNTs.add(new Point(lin,col));
+		   		break;
+		   	case BOAT:
+		   		listBoats.add(new Point(lin,col));
+		   		break;
+		   	case GOLD:
+		   		goldPos = new Point(lin,col);
+		   		break;
+		   		
+		   }
+	   }
 	   
 	   
 	   map[lin][col] = ch;
@@ -155,20 +252,54 @@ public class MapModel {
 		if(dir < 0) dir += 4;	
 		
 		break;
+		
+		case 'B':
+			if(numberTNT > 0) numberTNT--;
+			break;
 	   }
    }
    
    private void moveFoward()
    {
-	   // It still needs to treat many cases that it actually can't move
+	   int nextLin = lin, nextCol = col;
 	   switch(dir)
 	   {
-		   case NORTH: lin--; break;
-	       case EAST:  col++; break;
-	       case SOUTH: lin++; break;
-	       case WEST:  col--; break;
+		   case NORTH: nextLin--; break;
+	       case EAST:  nextCol++; break;
+	       case SOUTH: nextLin++; break;
+	       case WEST:  nextCol--; break;
 	   }
+	   char ch = map[nextLin][nextCol];
+	   if(ch == WALL || ch == TREE) return;
+	   if(ch == WATER && !inBoat) return;
+	   if(ch != WALL && ch != TREE && inBoat)
+	   {
+		   inBoat = false;
+		   listBoats.add(new Point(nextLin,nextCol));
+	   }
+	   else if(ch == BOAT && !inBoat)
+	   {
+		   inBoat = true;
+		   listBoats.remove(new Point(nextLin,nextCol));
+	   }
+	   else if(ch == AXE)
+	   {		   
+		   hasAxe = true;
+		   listAxes.remove(new Point(nextLin,nextCol));
+	   }
+	   else if(ch == TNT)
+	   {
+		   numberTNT++;
+		   listTNTs.remove(new Point(nextLin,nextCol)); 
+	   }
+	   else if(ch == GOLD) hasGold = true;
+		   
+		   
+		   
+	   lin = nextLin; col = nextCol;
+	   
    }
+   
    
    
    
---
+++ src/Point.java
@@ -0,0 +1,37 @@
+
+public class Point {
+	
+	public int lin, col;
+	
+	Point()
+	{
+		lin = col = 0;
+	}
+	
+	Point(int lin, int col)
+	{
+		this.lin = lin;
+		this.col = col;
+	}
+	
+	// Overriding equals() to compare two Complex objects
+    @Override
+    public boolean equals(Object o) {
+ 
+        // If the object is compared with itself then return true  
+        if (o == this) {
+            return true;
+        }
+        
+        if (!(o instanceof Point)) {
+            return false;
+        }
+
+        Point p = (Point) o;
+        return ( (this.lin == p.lin) &&
+        		 (this.col == p.col) );
+    }
+	
+	
+
+}
